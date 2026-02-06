aa98c7cff1f06f19bee41c4e98df28a10e85aa90
Aksh <jaiswal.aksh2005@gmail.com>
2026-02-06 14:23:55 +0530

    react

---
+++ appdemo/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import FunctionalComponent from './component/FunctionalComponent';
+function App() {
+  return (
+    <div className="App">
+      <h2>React App</h2>
+      <FunctionalComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ appdemo/src/component/FunctionalComponent.js
@@ -0,0 +1,9 @@
+function FunctionalComponent(){
+    return (
+        <div>
+            <h2>Functional Component</h2>
+            <p>Welcome to react application</p>
+        </div>
+    )
+}
+export default FunctionalComponent
